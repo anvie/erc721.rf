88a55cfcce765ae74fc311ea4c23489c175bcfad
robin <robin@ansvia.com>
2022-07-23 00:46:45 +0700

    add trailing /

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -11,4 +11,4 @@ def main():
     print("using owner key:", owner.address)
 
     print("Deploying $name$...")
-    return $name_pascal_case$.deploy("https://meta.chainbox.id/$param.project_id$", admin, {'from': owner})
+    return $name_pascal_case$.deploy("https://meta.chainbox.id/$param.project_id$/", admin, {'from': owner})
