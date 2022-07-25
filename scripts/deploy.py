997e05a4f65bc4eec69736650479890df3f8f48a
robin <robin@ansvia.com>
2022-07-26 00:12:47 +0700

    generate standard json input after deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 from brownie import $name_pascal_case$, accounts, config
+import json
 
+BASE_URL="https://meta.chainbox.id/$param.project_id$/"
 
 def main():
     owner = accounts.add(config['wallets']['owner_key'])
@@ -11,4 +13,12 @@ def main():
     print("using owner key:", owner.address)
 
     print("Deploying $name$...")
-    return $name_pascal_case$.deploy("https://$name_snake_case$.xyz/metadata/", admin, {'from': owner})
+    contract = $name_pascal_case$.deploy(BASE_URL, admin, {'from': owner}, publish_source=config['publish_source'])
+
+    print("Contract address:", contract.address)
+
+    standard_json_input = $name_pascal_case$.get_verification_info()['standard_json_input']
+    abi_file_name = '$name_pascal_case$-$param.project_id$-standard-input.json'
+    with open(abi_file_name, 'w') as f:
+      f.write(json.dumps(standard_json_input))
+    print(abi_file_name, "saved.")
